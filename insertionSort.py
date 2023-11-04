9185c52a8372a5e3059d87bf9016599e0640abbe
Ddivineconcepts <divinechinepoh@gmail.com>
2023-11-04 08:35:35 +0100

    insertionSortComplete

--- insertionSort.py
+++ insertionSort.py
@@ -4,10 +4,12 @@ def insertionSort(a, n):
    for i in range(n):
       next = a[i]
       j =i-1
-      for j in range(i-1,0, -1):
-         if j>=0 and a[j]:
-            a[j+1] = a[j]
+      while j>=0 and a[j] > next:
+         a[j+1] = a[j]
+         j -= 1
       a[j+1] = next
+      
+   return a
 
 def main():
    a =  [5,1,3,8,4,6,2,9]
